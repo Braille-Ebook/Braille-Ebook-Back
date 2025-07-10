c3ee417382d7fde36d674338365b24f12a60cddd
soyyeon <thdus8681@sookmyung.ac.kr>
2025-07-10 23:08:10 +0900

    #1 [CHORE] 백엔드 초기 셋팅 완료

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 4,              
+  arrowParens: 'always',     
+  bracketSameLine: false,    
+  bracketSpacing: true,      
+  jsxSingleQuote: true,      
+  singleQuote: true,         
+  trailingComma: 'es5',      
+  semi: true,                
+};
