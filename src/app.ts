7d91115caacd10056382ca3ed693f33d6802da49
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-07-15 16:01:16 +0900

    #6 [FEAT] morgan 미들웨어와 타입 설치

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.send('Hello TypeScript Backend!');
+    res.send('Hello TypeScript Backend!');
 });
 
 export default app;
