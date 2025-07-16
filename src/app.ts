c5a5d2043b9fe71d37da887c66a3fbbe426aacf5
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-07-16 17:05:28 +0900

    #6 [FEAT] express-session, cookie-parser 설치
    - 에러 미들웨어 생성
    - app.ts의 미들웨어 순서 바꿈
    - Error의 타입 확장
    - package.json의 start 커맨드 바꿈

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,11 @@
-import express from 'express';
+import express, { ErrorRequestHandler } from 'express';
+import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import session from 'express-session';
 import sequelize from './sequelize';
 import './models';
 
 const app = express();
-app.use(express.json());
-app.use(morgan('dev'));
-
-app.get('/', (req, res) => {
-    res.send('Hello TypeScript Backend!');
-});
 
 export const syncDB = async () => {
     try {
@@ -19,5 +15,36 @@ export const syncDB = async () => {
         console.error('DB 동기화 실패:', err);
     }
 };
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.COOKIE_SECRET!,
+        cookie: {
+            httpOnly: true,
+            secure: false,
+        },
+    })
+);
+
+app.get('/', (req, res) => {
+    res.send('Hello TypeScript Backend!');
+});
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    console.error(err);
+    res.status(err.status || 500);
+};
+app.use(errorHandler);
 
 export default app;
