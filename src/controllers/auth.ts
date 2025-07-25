d1770e446b9db9871bf93c11a98b055821b3d87e
soyyeon <thdus8681@sookmyung.ac.kr>
2025-07-25 17:43:40 +0900

    #8 [FEAT] response 변경

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -9,11 +9,17 @@ export const join = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const exUser = await User.findOne({ where: { email } });
         if (exUser) {
-            return res.redirect('/join?error=exist'); //유저 있으면 에러
+            return res.status(400).json({
+                success: false,
+                message: '이미 가입된 이메일입니다.',
+            }); //유저 있으면 에러
         }
 
         if (!password || password.length < 6) {
-            return res.redirect('/join?error=shortpassword'); //비밀번호 최소 6자
+            return res.status(400).json({
+                success: false,
+                message: '비밀번호는 6자 이상이어야 합니다.',
+            }); //비밀번호 최소 6자
         }
         const hash = await bcrypt.hash(password, 12); //비밀번호 암호화
 
@@ -24,7 +30,10 @@ export const join = async (req: Request, res: Response, next: NextFunction) => {
             provider: 'local',
         }); //회원정보 저장
 
-        return res.redirect('/');
+        return res.status(201).json({
+            success: true,
+            message: '회원가입이 완료되었습니다.',
+        });
     } catch (error) {
         console.error(error);
         return next(error);
@@ -45,7 +54,9 @@ export const login = (req: Request, res: Response, next: NextFunction) => {
             }
 
             if (!user) {
-                return res.redirect(`/?loginError=${info.message}`);
+                return res
+                    .status(401)
+                    .json({ success: false, message: info.message });
             }
 
             return req.login(user, (loginError) => {
@@ -54,7 +65,17 @@ export const login = (req: Request, res: Response, next: NextFunction) => {
                     return next(loginError);
                 }
 
-                return res.redirect('/');
+                const { user_id, email, nickname } = user as User;
+
+                return res.status(200).json({
+                    success: true,
+                    message: '로그인 성공',
+                    user: {
+                        id: user_id,
+                        email,
+                        nickname,
+                    },
+                });
             });
         }
     )(req, res, next);
@@ -62,6 +83,9 @@ export const login = (req: Request, res: Response, next: NextFunction) => {
 
 export const logout = (req: Request, res: Response) => {
     req.logout(() => {
-        res.redirect('/');
+        return res.status(200).json({
+            success: true,
+            message: '로그아웃 되었습니다.',
+        });
     });
 };
