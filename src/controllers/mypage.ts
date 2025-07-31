40f03d97f98b83e010bdc0102b4a973ee0fcbb3e
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-07-31 20:56:09 +0900

    #15 [FEAT] 내가 읽었던 책들 불러오기 API 생성

--- src/controllers/mypage.ts
+++ src/controllers/mypage.ts
@@ -67,4 +67,33 @@ const getMyReviews: RequestHandler = async (req, res, next) => {
     });
 };
 
-export { getMyInfo, getMyReviews };
+const getMyBooks: RequestHandler = async (req, res, next) => {
+    const progress = await UserBookProgress.findAll({
+        where: {
+            user_id: req.user!.user_id,
+        },
+        order: [['updated_at', 'DESC']],
+    });
+    const result = await Promise.all(
+        progress.map(async (progress) => {
+            const book = await Book.findByPk(progress.dataValues.book_id, {
+                attributes: ['title', 'author', 'translator'],
+            });
+            return {
+                bookId: progress.dataValues.book_id,
+                title: book?.dataValues.title,
+                author: book?.dataValues.author,
+                translator: book?.dataValues.translator,
+                updatedAt: progress.dataValues.updated_at,
+                isBookmarked: progress.dataValues.is_bookmarked,
+            };
+        })
+    );
+    res.status(200).send({
+        success: true,
+        message: '책 정보 불러오기가 성공했습니다.',
+        data: result,
+    });
+};
+
+export { getMyInfo, getMyReviews, getMyBooks };
--- src/routes/mypage.ts
+++ src/routes/mypage.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getMyInfo, getMyReviews } from '../controllers/mypage';
+import { getMyInfo, getMyReviews, getMyBooks } from '../controllers/mypage';
 import { isLoggedIn } from '../middlewares';
 
 const router = express.Router();
 
 router.get('/info', isLoggedIn, getMyInfo);
 router.get('/reviews', isLoggedIn, getMyReviews);
+router.get('/books', isLoggedIn, getMyBooks);
 
 export default router;
